c192adcc4edb2c8c9704051632a1ca0efa9a33e8
oAndin <00andherson@gmail.com>
2023-05-10 20:04:30 -0300

    Finalização do JS, claro que ia dar errado

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hello World!");
+
+const btnEl = document.querySelector(".btn");
+const bodyEl = document.querySelector("body");
+
+var colors = ["green","blue","red","orange","yellow","violet",]
+
+bodyEl.style.background = "pink";
+// #545454
+
+btnEl.addEventListener("click",addColor);
+
+function addColor(){
+    console.log("click");
+    var color = parseInt(Math.random()*VideoColorSpace.lenght);
+    bodyEl.style.background = colors[color];
+    count++;
+}
+console.log(colors);
