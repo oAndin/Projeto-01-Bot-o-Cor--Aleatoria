034a5b4c4dc81afbdccad73608b8b578fe000e01
oAndin <00andherson@gmail.com>
2023-05-16 20:08:25 -0300

    Gustavo é muito genio

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
+// // Criando três VAR para gerar as cores aleatorias!
+
+// var R = parseInt(Math.random() * 256);
+// var G = parseInt(Math.random() * 256);
+// var B = parseInt(Math.random() * 256);
+
+// console.log(R,G,B);
+
+// // Criando uma const RGB para gerar a cor aleatoria!
+
+// const RGB = `rgb(${R},${G},${B})`;
+
+// console.log(RGB);
+
+// ----------------------------------------------------------------------------------------------
+
 console.log("Hello World!");
 
 const btnEl = document.querySelector(".btn");
 const bodyEl = document.querySelector("body");
 
-var colors = ["green","blue","red","orange","yellow","violet",]
-
-bodyEl.style.background = "pink";
+bodyEl.style.background = "#545454";
 // #545454
 
 btnEl.addEventListener("click",addColor);
 
 function addColor(){
     console.log("clicked");
-    var color = parseInt(Math.random() * colors.length);
-    bodyEl.style.background = colors[color];
+    var R = parseInt(Math.random() * 256);
+    console.log(R);
+    var G = parseInt(Math.random() * 256);
+    console.log(G);
+    var B = parseInt(Math.random() * 256);
+    console.log(B);
+    const RGB = `rgb(${R},${G},${B})`;
+    bodyEl.style.background = RGB;
     var count = count++;
 }
-console.log(colors);
+
+// ----------------------------------------------------------------------------------------------
+
+
